b9b2a9f263641866899482649d658e19a1749b4c
Nils Jonsson <nils@alumni.rice.edu>
2017-05-06 20:27:05 -0500

    Generate project skeleton with Mix under Elixir v1.4.2 and Erlang v18

---
+++ lib/harmonex.ex
@@ -0,0 +1,18 @@
+defmodule Harmonex do
+  @moduledoc """
+  Documentation for Harmonex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Harmonex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/harmonex_test.exs
@@ -0,0 +1,8 @@
+defmodule HarmonexTest do
+  use ExUnit.Case
+  doctest Harmonex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
