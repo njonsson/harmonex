d55b8902d633a7d8d3189f7755b7b85d35acceab
Nils Jonsson <nils@alumni.rice.edu>
2017-05-07 00:37:47 -0500

    Implement detection of pitch alterations in Pitch.new/1 and refactor

--- lib/harmonex/pitch.ex
+++ lib/harmonex/pitch.ex
@@ -8,10 +8,10 @@ defmodule Harmonex.Pitch do
   @type t :: term
 
   @type bare_name :: :a | :b | :c | :d | :e | :f | :g
-  @bare_names      ~w(a    b    c    d    e    f    g)a
+  @bare_names     ~w( a    b    c    d    e    f    g )a
 
   @type alteration :: :natural | :flat | :sharp | :double_flat | :double_sharp
-  @alteration       ~w(natural    flat    sharp    double_flat    double_sharp)a
+  @alterations     ~w( natural    flat    sharp    double_flat    double_sharp )a
 
   @doc """
   Constructs a new `Harmonex.Pitch`.
@@ -21,23 +21,39 @@ defmodule Harmonex.Pitch do
       iex> Harmonex.Pitch.new :a
       %Harmonex.Pitch{bare_name: :a, alteration: :natural}
 
+      iex> Harmonex.Pitch.new :a, :flat
+      %Harmonex.Pitch{bare_name: :a, alteration: :flat}
+
+      iex> Harmonex.Pitch.new :a_flat
+      %Harmonex.Pitch{bare_name: :a, alteration: :flat}
+
       iex> Harmonex.Pitch.new :h
-      {:error, "Invalid pitch bare name -- must be an atom between :a and :g"}
+      {:error, "Invalid pitch name -- must be in #{inspect @bare_names} with an optional alteration (e.g, :a_flat)"}
 
       iex> Harmonex.Pitch.new :a, :out_of_tune
-      {:error, "Invalid pitch alteration -- must be :natural, :flat, :sharp, :double_flat, or :double_sharp"}
+      {:error, "Invalid pitch alteration -- must be in #{inspect @alterations}"}
   """
+  @spec new(bare_name | atom) :: t | {:error, binary}
   @spec new(bare_name, alteration) :: t | {:error, binary}
-  def new(bare_name, alteration \\ :natural) do
-    if bare_name in @bare_names do
-      if alteration in @alteration do
-        __MODULE__ |> struct(bare_name: bare_name, alteration: alteration)
-      else
-        {:error,
-         "Invalid pitch alteration -- must be :natural, :flat, :sharp, :double_flat, or :double_sharp"}
+  for bare_name <- @bare_names do
+    def new(unquote(bare_name)), do: new(unquote(bare_name), :natural)
+
+    for alteration <- @alterations do
+      def new(unquote(bare_name), unquote(alteration)) do
+        __MODULE__ |> struct(bare_name: unquote(bare_name),
+                             alteration: unquote(alteration))
       end
-    else
-      {:error, "Invalid pitch bare name -- must be an atom between :a and :g"}
+
+      name = String.to_atom(to_string(bare_name) <> "_" <> to_string(alteration))
+      def new(unquote(name)), do: new(unquote(bare_name), unquote(alteration))
     end
+
+    def new(unquote(bare_name), _) do
+      {:error, "Invalid pitch alteration -- must be in #{inspect @alterations}"}
+    end
+  end
+
+  def new(_) do
+    {:error, "Invalid pitch name -- must be in #{inspect @bare_names} with an optional alteration (e.g, :a_flat)"}
   end
 end
