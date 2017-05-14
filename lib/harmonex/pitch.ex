ef866517a57ed31c5ff2ee60ab0167fcb80bbf5b
Nils Jonsson <nils@alumni.rice.edu>
2017-05-14 00:07:03 -0500

    Expand test coverage and implement error handling for Pitch functions

--- lib/harmonex/pitch.ex
+++ lib/harmonex/pitch.ex
@@ -33,57 +33,87 @@ defmodule Harmonex.Pitch do
 
   @type semitones :: 0..11
 
+  @invalid_name "Invalid pitch name -- must be in #{inspect @bare_names}"
+  @invalid_alteration "Invalid pitch alteration -- must be in #{inspect @alterations}"
+
   @doc """
   Computes the alteration of the specified `pitch`.
 
   ## Examples
 
+      iex> Harmonex.Pitch.alteration %{bare_name: :a, alteration: :flat}
+      :flat
+
+      iex> Harmonex.Pitch.alteration %{bare_name: :a}
+      :natural
+
       iex> Harmonex.Pitch.alteration %{alteration: :flat}
+      :flat
+
+      iex> Harmonex.Pitch.alteration :a_flat
       :flat
 
       iex> Harmonex.Pitch.alteration :a
       :natural
-
-      iex> Harmonex.Pitch.alteration :a_flat
-      :flat
   """
   @spec alteration(%{alteration: alteration} | atom) :: alteration
-  def alteration(%{alteration: alteration}=_pitch), do: alteration
+
+  for alteration <- @alterations do
+    def alteration(%{alteration: unquote(alteration)=alteration}=_pitch) do
+      alteration
+    end
+  end
+
+  def alteration(%{alteration: _}=_pitch), do: {:error, @invalid_alteration}
 
   for bare_name <- @bare_names do
-    def alteration(unquote(bare_name)=_pitch), do: :natural
-
     for alteration <- @alterations do
       full_name = String.to_atom("#{to_string bare_name}_#{to_string alteration}")
       def alteration(unquote(full_name)=_pitch), do: unquote(alteration)
     end
+
+    def alteration(%{bare_name: unquote(bare_name)}=_pitch), do: :natural
+
+    def alteration(unquote(bare_name)=_pitch), do: :natural
   end
+
+  def alteration(_pitch), do: {:error, @invalid_name}
 
   @doc """
   Computes the bare name of the specified `pitch`.
 
   ## Examples
 
-      iex> Harmonex.Pitch.bare_name %{bare_name: :a}
+      iex> Harmonex.Pitch.bare_name %{bare_name: :a, alteration: :flat}
       :a
 
-      iex> Harmonex.Pitch.bare_name :a
+      iex> Harmonex.Pitch.bare_name %{bare_name: :a}
       :a
 
       iex> Harmonex.Pitch.bare_name :a_flat
       :a
+
+      iex> Harmonex.Pitch.bare_name :a
+      :a
   """
-  @spec bare_name(%{bare_name: bare_name} | atom) :: bare_name
-  def bare_name(%{bare_name: bare_name}=_pitch), do: bare_name
+  @spec bare_name(t) :: bare_name
 
   for bare_name <- @bare_names do
-    def bare_name(unquote(bare_name)=pitch), do: pitch
+    def bare_name(%{bare_name: unquote(bare_name)=bare_name}), do: bare_name
 
+    def bare_name(unquote(bare_name)=bare_name), do: bare_name
+  end
+
+  def bare_name(%{bare_name: _}), do: {:error, @invalid_name}
+
+  for bare_name <- @bare_names do
     for alteration <- @alterations do
       full_name = String.to_atom("#{to_string bare_name}_#{to_string alteration}")
       def bare_name(unquote(full_name)=_pitch), do: unquote(bare_name)
     end
   end
+
+  def bare_name(_pitch), do: {:error, @invalid_name}
 
   @doc """
   Determines whether the specified `pitch1` and `pitch2` are enharmonically
@@ -100,14 +130,15 @@ defmodule Harmonex.Pitch do
       iex> Harmonex.Pitch.enharmonic? :b_sharp, :d_double_flat
       true
 
-      iex> Harmonex.Pitch.enharmonic? :a_sharp, :a_natural
+      iex> Harmonex.Pitch.enharmonic? :a_sharp, :a
       false
   """
   @spec enharmonic?(t, t) :: boolean
   def enharmonic?(pitch1, pitch2) do
-    index1 = pitch1 |> full_name |> index_chromatic
-    index2 = pitch2 |> full_name |> index_chromatic
-    index1 == index2
+    with pitch1_full_name when is_atom(pitch1_full_name) <- full_name(pitch1),
+         pitch2_full_name when is_atom(pitch2_full_name) <- full_name(pitch2) do
+      index_chromatic(pitch1_full_name) == index_chromatic(pitch2_full_name)
+    end
   end
 
   @doc """
@@ -121,6 +152,9 @@ defmodule Harmonex.Pitch do
       iex> Harmonex.Pitch.enharmonics %{bare_name: :g, alteration: :sharp}
       [%Harmonex.Pitch{bare_name: :a, alteration: :flat}]
 
+      iex> Harmonex.Pitch.enharmonics :g
+      [:f_double_sharp, :a_double_flat]
+
       iex> Harmonex.Pitch.enharmonics :c_flat
       [:a_double_sharp, :b_natural]
 
@@ -132,14 +166,17 @@ defmodule Harmonex.Pitch do
   """
   @spec enharmonics(t) :: [t]
   def enharmonics(%{bare_name: _}=pitch) do
-    pitch |> full_name |> enharmonics |> Enum.map(&(new(&1)))
+    with pitch_full_name when is_atom(pitch_full_name) <- full_name(pitch) do
+      pitch_full_name |> enharmonics |> Enum.map(&(new(&1)))
+    end
   end
 
   def enharmonics(pitch) do
-    pitch_full_name = pitch |> full_name
-    pitch_full_name |> index_chromatic
-                    |> full_names_at
-                    |> Enum.reject(&(&1 == pitch_full_name))
+    with pitch_full_name when is_atom(pitch_full_name) <- full_name(pitch) do
+      pitch_full_name |> index_chromatic
+                      |> full_names_at
+                      |> Enum.reject(&(&1 == pitch_full_name))
+    end
   end
 
   @doc """
@@ -153,17 +190,17 @@ defmodule Harmonex.Pitch do
       iex> Harmonex.Pitch.full_name %{bare_name: :a}
       :a_natural
 
-      iex> Harmonex.Pitch.full_name :a
-      :a_natural
-
       iex> Harmonex.Pitch.full_name :a_flat
       :a_flat
+
+      iex> Harmonex.Pitch.full_name :a
+      :a_natural
   """
   @spec full_name(t) :: atom
 
   for bare_name <- @bare_names do
     for alteration <- @alterations do
-      full_name = String.to_atom("#{to_string bare_name}_#{to_string alteration}")
+      full_name = :"#{to_string bare_name}_#{to_string alteration}"
       def full_name(%{bare_name: unquote(bare_name),
                       alteration: unquote(alteration)}=_pitch) do
         unquote full_name
@@ -172,12 +209,20 @@ defmodule Harmonex.Pitch do
       def full_name(unquote(full_name)=pitch), do: pitch
     end
 
+    def full_name(%{bare_name: unquote(bare_name), alteration: _}=_pitch) do
+      {:error, @invalid_alteration}
+    end
+
     def full_name(%{bare_name: unquote(bare_name)}=_pitch) do
       unquote :"#{to_string bare_name}_natural"
     end
 
-    def full_name(unquote(bare_name)=pitch), do: :"#{to_string pitch}_natural"
+    def full_name(unquote(bare_name)=_pitch) do
+      unquote(:"#{to_string bare_name}_natural")
+    end
   end
+
+  def full_name(_pitch), do: {:error, @invalid_name}
 
   @doc """
   Computes the quality and number of the interval between the specified
@@ -205,13 +250,12 @@ defmodule Harmonex.Pitch do
   """
   @spec interval_diatonic(t, t) :: interval_diatonic | {:error, binary}
   def interval_diatonic(low_pitch, high_pitch) do
-    semitones = semitones(low_pitch, high_pitch)
-
-    low_staff_position  = low_pitch  |> bare_name |> staff_position
-    high_staff_position = high_pitch |> bare_name |> staff_position
-    number = Integer.mod(high_staff_position - low_staff_position, 7) + 1
-
-    semitones_and_number_to_interval_diatonic semitones, number
+    with semitones when is_integer(semitones) <- semitones(low_pitch, high_pitch) do
+      low_staff_position  = low_pitch  |> bare_name |> staff_position
+      high_staff_position = high_pitch |> bare_name |> staff_position
+      number = Integer.mod(high_staff_position - low_staff_position, 7) + 1
+      semitones_and_number_to_interval_diatonic semitones, number
+    end
   end
 
   @doc """
@@ -222,18 +266,35 @@ defmodule Harmonex.Pitch do
       iex> Harmonex.Pitch.new :a
       %Harmonex.Pitch{bare_name: :a, alteration: :natural}
 
+      iex> Harmonex.Pitch.new %{bare_name: :a}
+      %Harmonex.Pitch{bare_name: :a, alteration: :natural}
+
       iex> Harmonex.Pitch.new :a_flat
       %Harmonex.Pitch{bare_name: :a, alteration: :flat}
 
+      iex> Harmonex.Pitch.new %{bare_name: :a, alteration: :flat}
+      %Harmonex.Pitch{bare_name: :a, alteration: :flat}
+
+      iex> Harmonex.Pitch.new %{bare_name: :h}
+      {:error, #{inspect @invalid_name}}
+
       iex> Harmonex.Pitch.new :h
-      {:error, "Invalid pitch name -- must be in #{inspect @bare_names} with an optional alteration (e.g, :a_flat)"}
+      {:error, #{inspect @invalid_name}}
+
+      iex> Harmonex.Pitch.new %{bare_name: :a, alteration: :out_of_tune}
+      {:error, #{inspect @invalid_alteration}}
   """
   @spec new(atom) :: t | {:error, binary}
   @spec new(bare_name, alteration) :: t | {:error, binary}
   for bare_name <- @bare_names do
-    def new(unquote(bare_name)=name), do: new(name, :natural)
+    def new(unquote(bare_name)=bare_name), do: new(bare_name, :natural)
 
     for alteration <- @alterations do
+      def new(%{bare_name: unquote(bare_name)=bare_name,
+                alteration: unquote(alteration)=alteration}) do
+        new bare_name, alteration
+      end
+
       @doc """
       Constructs a new `Harmonex.Pitch` with the specified `bare_name` and
       `alteration`.
@@ -243,28 +304,38 @@ defmodule Harmonex.Pitch do
           iex> Harmonex.Pitch.new :a, :flat
           %Harmonex.Pitch{bare_name: :a, alteration: :flat}
 
+          iex> Harmonex.Pitch.new :h, :flat
+          {:error, #{inspect @invalid_name}}
+
           iex> Harmonex.Pitch.new :a, :out_of_tune
-          {:error, "Invalid pitch alteration -- must be in #{inspect @alterations}"}
+          {:error, #{inspect @invalid_alteration}}
       """
       def new(unquote(bare_name)=bare_name, unquote(alteration)=alteration) do
         __MODULE__ |> struct(bare_name: bare_name, alteration: alteration)
       end
 
-      full_name = String.to_atom("#{to_string bare_name}_#{to_string alteration}")
+      full_name = :"#{to_string bare_name}_#{to_string alteration}"
       def new(unquote(full_name)=_full_name) do
-        new(unquote(bare_name), unquote(alteration))
+        new unquote(bare_name), unquote(alteration)
       end
     end
 
+    def new(%{bare_name: unquote(bare_name), alteration: _}) do
+      {:error, @invalid_alteration}
+    end
+
+    def new(%{bare_name: unquote(bare_name)=bare_name}) do
+      new bare_name, :natural
+    end
+
     def new(unquote(bare_name)=_bare_name, _invalid_alteration) do
-      {:error, "Invalid pitch alteration -- must be in #{inspect @alterations}"}
+      {:error, @invalid_alteration}
     end
   end
 
-  def new(_invalid_name) do
-    {:error,
-     "Invalid pitch name -- must be in #{inspect @bare_names} with an optional alteration (e.g, :a_flat)"}
-  end
+  def new(_invalid_name, _alteration), do: {:error, @invalid_name}
+
+  def new(_invalid_name), do: {:error, @invalid_name}
 
   @doc """
   Computes the distance in half steps between the specified `low_pitch` and
@@ -283,23 +354,23 @@ defmodule Harmonex.Pitch do
   """
   @spec semitones(t, t) :: semitones
   def semitones(low_pitch, high_pitch) do
-    low_index  = low_pitch  |> full_name |> index_chromatic
-    high_index = high_pitch |> full_name |> index_chromatic
-    (high_index - low_index) |> Integer.mod(12)
+    with low_full when is_atom(low_full)   <- full_name(low_pitch),
+         low_index                         <- index_chromatic(low_full),
+         high_full when is_atom(high_full) <- full_name(high_pitch),
+         high_index                        <- index_chromatic(high_full) do
+      (high_index - low_index) |> Integer.mod(12)
+    end
   end
 
   @spec index_chromatic(atom) :: semitones
   indexes_by_bare_name = [c: 0, d: 2, e: 4, f: 5, g: 7, a: 9, b: 11]
   for {bare_name, index} <- indexes_by_bare_name do
     defp index_chromatic(unquote(bare_name)), do: unquote(index)
-  end
 
-  for bare_name <- @bare_names do
     for {alteration, offset} <- @alteration_offsets do
-      full_name = String.to_atom("#{to_string bare_name}_#{to_string alteration}")
+      full_name = :"#{to_string bare_name}_#{to_string alteration}"
       defp index_chromatic(unquote(full_name)) do
-        index_chromatic = index_chromatic(unquote(bare_name))
-        (index_chromatic + unquote(offset)) |> Integer.mod(12)
+        unquote(index + offset) |> Integer.mod(12)
       end
     end
   end
@@ -311,7 +382,7 @@ defmodule Harmonex.Pitch do
                                                                     acc) ->
     @alteration_offsets |> Enum.reduce(acc,
                                        fn({alteration, offset}, inner_acc) ->
-      full_name = String.to_atom("#{to_string bare_name}_#{to_string alteration}")
+      full_name = :"#{to_string bare_name}_#{to_string alteration}"
       altered_index = Integer.mod(index + offset, 12)
       insert_at = if full_name == :b_flat, do: -1, else: 0
       inner_acc |> Map.put_new(altered_index, [])
--- test/harmonex/pitch_test.exs
+++ test/harmonex/pitch_test.exs
@@ -1,4 +1,221 @@
 defmodule Harmonex.PitchTest do
   use ExUnit.Case
   doctest Harmonex.Pitch
+
+  @invalid_name "Invalid pitch name -- must be in [:a, :b, :c, :d, :e, :f, :g]"
+  @invalid_alteration "Invalid pitch alteration -- must be in [:double_flat, :flat, :natural, :sharp, :double_sharp]"
+
+  describe ".alteration/1" do
+    test "rejects an invalid alteration" do
+      expected = {:error, @invalid_alteration}
+
+      actual = Harmonex.Pitch.alteration(%Harmonex.Pitch{bare_name: :a,
+                                                         alteration: :out_of_tune})
+      assert actual == expected
+
+      actual = Harmonex.Pitch.alteration(%Harmonex.Pitch{alteration: :out_of_tune})
+      assert actual == expected
+    end
+
+    test "rejects an invalid name" do
+      expected = {:error, @invalid_name}
+      actual = Harmonex.Pitch.alteration(:a_out_of_tune)
+      assert actual == expected
+    end
+  end
+
+  describe ".bare_name/1" do
+    test "rejects an invalid name" do
+      expected = {:error, @invalid_name}
+
+      actual = Harmonex.Pitch.bare_name(%Harmonex.Pitch{bare_name: :h,
+                                                        alteration: :flat})
+      assert actual == expected
+
+      actual = Harmonex.Pitch.bare_name(%Harmonex.Pitch{bare_name: :h})
+      assert actual == expected
+
+      actual = Harmonex.Pitch.bare_name(:h_flat)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.bare_name(:h)
+      assert actual == expected
+    end
+  end
+
+  describe ".enharmonic?/2" do
+    test "rejects an invalid name in the first argument" do
+      expected = {:error, @invalid_name}
+
+      actual = Harmonex.Pitch.enharmonic?(%Harmonex.Pitch{bare_name: :h,
+                                                          alteration: :flat},
+                                          :a)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.enharmonic?(%Harmonex.Pitch{bare_name: :h}, :a)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.enharmonic?(:h_flat, :a)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.enharmonic?(:h, :a)
+      assert actual == expected
+    end
+
+    test "rejects an invalid name in the second argument" do
+      expected = {:error, @invalid_name}
+
+      actual = Harmonex.Pitch.enharmonic?(:a,
+                                          %Harmonex.Pitch{bare_name: :h,
+                                                          alteration: :flat})
+      assert actual == expected
+
+      actual = Harmonex.Pitch.enharmonic?(:a, %Harmonex.Pitch{bare_name: :h})
+      assert actual == expected
+
+      actual = Harmonex.Pitch.enharmonic?(:a, :h_flat)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.enharmonic?(:a, :h)
+      assert actual == expected
+    end
+  end
+
+  describe ".enharmonics/1" do
+    test "rejects an invalid name" do
+      expected = {:error, @invalid_name}
+
+      actual = Harmonex.Pitch.enharmonics(%Harmonex.Pitch{bare_name: :h,
+                                                          alteration: :flat})
+      assert actual == expected
+
+      actual = Harmonex.Pitch.enharmonics(%Harmonex.Pitch{bare_name: :h})
+      assert actual == expected
+
+      actual = Harmonex.Pitch.enharmonics(:h_flat)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.enharmonics(:h)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.enharmonics(:a_out_of_tune)
+      assert actual == expected
+    end
+
+    test "rejects an invalid alteration" do
+      expected = {:error, @invalid_alteration}
+
+      actual = Harmonex.Pitch.enharmonics(%Harmonex.Pitch{bare_name: :a,
+                                                          alteration: :out_of_tune})
+      assert actual == expected
+    end
+  end
+
+  describe ".full_name/1" do
+    test "rejects an invalid name" do
+      expected = {:error, @invalid_name}
+
+      actual = Harmonex.Pitch.full_name(%Harmonex.Pitch{bare_name: :h,
+                                                        alteration: :flat})
+      assert actual == expected
+
+      actual = Harmonex.Pitch.full_name(%Harmonex.Pitch{bare_name: :h})
+      assert actual == expected
+
+      actual = Harmonex.Pitch.full_name(:h_flat)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.full_name(:h)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.full_name(:a_out_of_tune)
+      assert actual == expected
+    end
+
+    test "rejects an invalid alteration" do
+      expected = {:error, @invalid_alteration}
+
+      actual = Harmonex.Pitch.full_name(%Harmonex.Pitch{bare_name: :a,
+                                                        alteration: :out_of_tune})
+      assert actual == expected
+    end
+  end
+
+  describe ".interval_diatonic/2" do
+    test "rejects an invalid name in the first argument" do
+      expected = {:error, @invalid_name}
+
+      actual = Harmonex.Pitch.interval_diatonic(%Harmonex.Pitch{bare_name: :h,
+                                                                alteration: :flat},
+                                                :a)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.interval_diatonic(%Harmonex.Pitch{bare_name: :h},
+                                                :a)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.interval_diatonic(:h_flat, :a)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.interval_diatonic(:h, :a)
+      assert actual == expected
+    end
+
+    test "rejects an invalid name in the second argument" do
+      expected = {:error, @invalid_name}
+
+      actual = Harmonex.Pitch.interval_diatonic(:a,
+                                                %Harmonex.Pitch{bare_name: :h,
+                                                                alteration: :flat})
+      assert actual == expected
+
+      actual = Harmonex.Pitch.interval_diatonic(:a,
+                                                %Harmonex.Pitch{bare_name: :h})
+      assert actual == expected
+
+      actual = Harmonex.Pitch.interval_diatonic(:a, :h_flat)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.interval_diatonic(:a, :h)
+      assert actual == expected
+    end
+  end
+
+  describe ".semitones/2" do
+    test "rejects an invalid name in the first argument" do
+      expected = {:error, @invalid_name}
+
+      actual = Harmonex.Pitch.semitones(%Harmonex.Pitch{bare_name: :h,
+                                                        alteration: :flat},
+                                        :a)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.semitones(%Harmonex.Pitch{bare_name: :h}, :a)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.semitones(:h_flat, :a)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.semitones(:h, :a)
+      assert actual == expected
+    end
+
+    test "rejects an invalid name in the second argument" do
+      expected = {:error, @invalid_name}
+
+      actual = Harmonex.Pitch.semitones(:a,
+                                        %Harmonex.Pitch{bare_name: :h,
+                                                        alteration: :flat})
+      assert actual == expected
+
+      actual = Harmonex.Pitch.semitones(:a, %Harmonex.Pitch{bare_name: :h})
+      assert actual == expected
+
+      actual = Harmonex.Pitch.semitones(:a, :h_flat)
+      assert actual == expected
+
+      actual = Harmonex.Pitch.semitones(:a, :h)
+      assert actual == expected
+    end
+  end
 end
